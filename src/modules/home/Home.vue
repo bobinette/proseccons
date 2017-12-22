9e88720fec4e35d39a02350a718526a11c3672e0
Bobinette <xavier.chassin@live.fr>
2017-12-22 19:43:36 +0100

    Add input to create a new proseccon

--- src/modules/home/Home.vue
+++ src/modules/home/Home.vue
@@ -2,13 +2,25 @@
   <div>
     <navbar></navbar>
     <div class="container">
+      <!-- Proseccons -->
       <pc-table
         v-for="(proseccon, idx) in proseccons"
+        class="ProsecconTable"
+        :key="idx"
         :title="proseccon.title"
         :pros="proseccon.pros"
         :cons="proseccon.cons"
         @new-item="(input) => newItem(idx, input)"
       ></pc-table>
+
+      <!-- New proseccon input -->
+      <input
+        type="text"
+        class="ProsecconInput"
+        v-model="title"
+        @keyup.13="newProseccon"
+        placeholder="Title of the new Proseccon, e.g. &quot;Should I buy that fancy Millenium Falcon?&quot;"
+      >
     </div>
   </div>
 </template>
@@ -36,6 +48,7 @@ export default {
   name: 'Home',
   data() {
     return {
+      title: '',
       proseccons: [
         {
           title: 'First PC',
@@ -63,6 +76,10 @@ export default {
     };
   },
   methods: {
+    newProseccon() {
+      this.proseccons.push({ title: this.title, pros: [], cons: [] });
+      this.item = '';
+    },
     newItem(idx, input) {
       const proseccon = parseItem(input);
       if (!proseccon || proseccon.score === 0) {
@@ -85,5 +102,11 @@ export default {
 </script>
 
 <style>
+.ProsecconTable {
+  margin-bottom: 2rem;
+}
 
+.ProsecconInput {
+  width: 100%;
+}
 </style>
